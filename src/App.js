1ba10d9181558581d1e38ee75cdd0e48da41c63d
KIM-DAE-KEUN <dk.kim33333@gmail.com>
2024-10-29 17:28:03 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
 import logo from './logo.svg';
+import Customer from './components/Customer'
 import './App.css';
+
+const customer = [
+  {
+    'id' : 1,
+    'image' : 'https://picsum.photos/id/1/100/100',
+    'name' : '나동빈' , 
+    'birthday' : '961222',
+    'gender' : '남자',
+    'job' : '대학생'
+  },
+  {
+    'id' : 2,
+    'image' : 'https://picsum.photos/id/2/100/100',
+    'name' : '홍길동' , 
+    'birthday' : '961222',
+    'gender' : '남자',
+    'job' : '대학생'
+  },
+  {
+    'id' : 3,
+    'image' : 'https://picsum.photos/id/3/100/100',
+    'name' : '홍길녀' , 
+    'birthday' : '960305',
+    'gender' : '여자',
+    'job' : '디자이너'
+  }
+];
 
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo"/>
-      <h2>Let's develop management</h2>
+    <div>
+      {
+        customer.map(cus => {
+          return (
+            <Customer 
+              key={cus.id}
+              id={cus.id} 
+              image={cus.image}
+              name={cus.name}
+              birthday={cus.birthday}
+              gender={cus.gender}
+              job={cus.job}
+            />  
+          ); 
+        })
+      }  
     </div>
   );
 }
